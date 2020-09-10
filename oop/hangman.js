053d51cc3b72f503aee4b2a7d248552b1e189eb8
I066841 <a.kulshreshtha@sap.com>
2020-09-10 23:41:28 +0530

    Hangman Challenge

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,10 +1,26 @@
 const Hangman = function (word, remainingGuesses) {
-    this.word = word
+    this.word = word.toLowerCase().split('')
     this.remainingGuesses = remainingGuesses
+    this.guessedLetters = ['c']
+}
+
+
+Hangman.prototype.getPuzzle = function() {
+    let puzzle = ''
+    
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter
+        } else {
+            puzzle += '*'
+        }
+    })
+
+    return puzzle
 }
 
 const game1 = new Hangman('cat', 2)
-console.log(game1)
+console.log(game1.getPuzzle())
 
-const game2 = new Hangman('mouse', 3)
-console.log(game2)
+const game2 = new Hangman('Hakunacatata', 6)
+console.log(game2.getPuzzle())
